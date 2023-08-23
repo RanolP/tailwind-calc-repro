d05a8d0310b76a6f1e09d8ab154744f234d06ef1
RanolP <ranolp@laftel.net>
2023-08-23 10:51:30 +0900

    initial commit

---
+++ emails/tailwind-calc-plus.tsx
@@ -0,0 +1,75 @@
+import { Html, Body, Tailwind } from '@react-email/components';
+import * as React from 'react';
+
+export const NotionMagicLinkEmail = () => (
+  <Tailwind>
+    <Html>
+      <Body className="bg-white">
+        <div className="max-h-[100px] bg-green-100">
+          <div className="h-[200px]">something tall</div>
+        </div>
+        <div className="max-h-[50px+50px] bg-red-100">
+          <div className="h-[200px]">something tall</div>
+        </div>
+      </Body>
+    </Html>
+  </Tailwind>
+);
+
+export default NotionMagicLinkEmail;
+
+const main = {
+  backgroundColor: '#ffffff',
+};
+
+const container = {
+  paddingLeft: '12px',
+  paddingRight: '12px',
+  margin: '0 auto',
+};
+
+const h1 = {
+  color: '#333',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: '24px',
+  fontWeight: 'bold',
+  margin: '40px 0',
+  padding: '0',
+};
+
+const link = {
+  color: '#2754C5',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: '14px',
+  textDecoration: 'underline',
+};
+
+const text = {
+  color: '#333',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: '14px',
+  margin: '24px 0',
+};
+
+const footer = {
+  color: '#898989',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: '12px',
+  lineHeight: '22px',
+  marginTop: '12px',
+  marginBottom: '24px',
+};
+
+const code = {
+  display: 'inline-block',
+  padding: '16px 4.5%',
+  width: '90.5%',
+  backgroundColor: '#f4f4f4',
+  borderRadius: '5px',
+  border: '1px solid #eee',
+  color: '#333',
+};
